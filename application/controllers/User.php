0fe09ec9da2eddd630e7d9a9f3ab9169ce73b2a9
AvikRaxit <avik.prof@gmail.com>
2023-02-08 13:12:13 +0530

    changes

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -3,7 +3,7 @@
 	class User extends CI_Controller
 	{
 		public function index() {
-			$this->load->model('Usermodel');
+			// $this->load->model('Usermodel'); // auto load to the autoload.php
 			$getdata['allusers'] = $this->Usermodel->getUserData();
 			// print_r($getdata);
 			$this->load->view('users/user_list',$getdata);
--- application/models/usermodel.php
+++ application/models/usermodel.php
@@ -3,9 +3,17 @@
 	{
 	  function getUserData()
 	  {
-	    $this->load->database();
-	    $result = $this->db->query("select * from user");
+	    // $this->load->database(); // auto load to the autoload.php
+		// $this->db->select('role'); // active record select query
+		// $this->db->where('role', 'ROLE_USER'); // active record where clause query
+	    //$result = $this->db->get("user");	// active record select all query
+
+		// $result = $this->db->where('role', 'ROLE_USER') 
+		// 				->get('user'); // Eloquent type 
+		$result = $this->db->get('user');
+
 	    // echo "<pre>";
+		// print_r($result);
 	    return $result->result_array();
 	  }
 }
